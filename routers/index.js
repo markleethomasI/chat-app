8fe13f457e12bb28b6997fb33e716cc53e7e8eb8
markleethomasI <108632435+markleethomasI@users.noreply.github.com>
2022-10-18 14:09:52 -0500

    Removed unneeded async function

--- routers/index.js
+++ routers/index.js
@@ -5,11 +5,11 @@ const path = require('path')
 // Use express.json middleware
 router.use(express.json())
 
-router.get('/chat-app/', async (req, res) => {
+router.get('/chat-app/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
 })
 
-router.get('/chat-app/chat', async (req, res) => {
+router.get('/chat-app/chat', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'chat.html'))
   })
 
